302b8000aac9d5142705db2a8baff752807db3ba
Harshit-087 <harshit283773@gmail.com>
2026-05-10 00:03:21 +0530

    chore:update nextconfig

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.137.117.232'],
+  
 };
 
 export default nextConfig;
